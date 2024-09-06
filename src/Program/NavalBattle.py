55bdac72a9fbcc7e4068634ab4da69553fa496dd
DilanUrrego <141243258+DilanUrrego@users.noreply.github.com>
2024-09-06 09:18:09 -0500

    Update NavalBattle.py

--- src/Program/NavalBattle.py
+++ src/Program/NavalBattle.py
@@ -40,10 +40,20 @@ class NavalBattle:
     COLUMNS = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"]
     FIXED_COLUMNS = [" A"," B"," C"," D"," E"," F"," G"," H"," I"," J"," K"," L"," M"," N"," O"," P"," Q"," R"," S"," T"]
     board = None
+    EMOJIS={
+    "colition" : "\U0001F4A5",
+    "water" : "\U0001F4A6",
+    "ship" :"\U0001F6A2",
+    "question" :"\U00002754",
+    "wave" : "\U0001F30A"
+    }
+    shooting_history=set()
 
-    def generateBoard(self, w=8, h=5):
+
+    
+    def validateBoardDimensions(self, w, h):
         """
-        Genera un tablero de juego con dimensiones especificadas.
+        Valida las dimensiones del tablero y lanza excepciones si no son válidas.
 
         Parámetros:
         w -- número de columnas del tablero.
@@ -62,10 +72,29 @@ class NavalBattle:
             raise BoardIsTooSmall
         elif w > 9 or h > 9:
             raise BoardIsTooBig
+
+
+    def generateBoard(self, w=8, h=5):
+        """
+        Genera un tablero de juego con dimensiones especificadas.
+
+        Parámetros:
+        w -- número de columnas del tablero.
+        h -- número de filas del tablero.
+
+        Retorna:
+        El tablero generado como una lista de listas.
+        """
+        # Validar las dimensiones antes de generar el tablero
+        self.validateBoardDimensions(w, h)
+        
         self.w = w
         self.h = h
         self.board = [[0 for j in range(w)] for i in range(h)] 
-        self.player_board = [["\U00002754" for i in range(w)] for i in range(h)]  # Tablero visual para el jugador
+        self.player_board = [[self.EMOJIS["question"] for i in range(w)] for i in range(h)]  # Tablero visual para el jugador
+
+        return self.board
+
 
         return self.board
     
@@ -98,8 +127,8 @@ class NavalBattle:
                 self.ships_quantity += 1
                 len -= 1
             else:
-                print("There's not enough space")
-
+                raise NotEnoughSpace
+            
         elif not vertical:
             # Verifica si hay suficiente espacio horizontalmente
             for s in range(len):
@@ -115,7 +144,7 @@ class NavalBattle:
                 self.ships_quantity += 1
                 len -= 1
             else:
-                print("There's not enough space")
+                raise NotEnoughSpace
              
     def addShips(self, num_ships, limit=8):
         """
@@ -132,7 +161,7 @@ class NavalBattle:
             raise NotEnoughSpace
         ship_len = num_ships + 1
         if self.board is None:
-            raise BoardError("There's no board yet")
+            raise BoardError
         # Intenta añadir barcos hasta alcanzar la cantidad deseada
         while self.ships_quantity < num_ships:
             enough_space = True
@@ -169,9 +198,9 @@ class NavalBattle:
                     self.ships_quantity += 1
                     ship_len -= 1
 
-    def shoot(self, coordinate: str):
+    def validateCoordinate(self, coordinate: str):
         """
-        Realiza un disparo a una coordenada específica.
+        Valida la coordenada proporcionada para un disparo.
 
         Parámetros:
         coordinate -- coordenada en formato "C2" (columna, fila).
@@ -179,45 +208,69 @@ class NavalBattle:
         Excepciones:
         Lanza InvalidCoordinate si la coordenada es inválida.
         Lanza RowOutOfRange y ColumnOutOfRange si la coordenada está fuera de los límites.
+
+        Retorna:
+        Una tupla con el índice de la fila y la columna válidos.
         """
         coordinate = coordinate.upper()
-        column = coordinate[0]
         if len(coordinate) != 2:
             raise InvalidCoordinate
-        row = int(coordinate[1]) - 1
+        
+        column = coordinate[0]
+        try:
+            row = int(coordinate[1]) - 1
+        except ValueError:
+            raise InvalidCoordinate
 
         # Verifica si la fila está en el rango válido
-        if row >= len(self.board):
+        if row < 0 or row >= len(self.board):
             raise RowOutOfRange
         
         # Verifica si la columna está en el rango válido
-        if column in self.COLUMNS:
-            column = self.COLUMNS.index(column)
-            if column >= len(self.board[0]):
-                raise ColumnOutOfRange
-        else:
+        if column not in self.COLUMNS:
             raise ColumnOutOfRange
         
-        # Verifica si la posición ya ha sido atacada
-        if self.board[row][column] == "x":
-            print("Don't waste your shots")
-            return False
+        column_index = self.COLUMNS.index(column)
+        if column_index >= len(self.board[0]):
+            raise ColumnOutOfRange
         
+        return row, column_index
+
+
+
+
+    def shoot(self, coordinate: str):
+        """
+        Realiza un disparo a una coordenada específica.
+
+        Parámetros:
+        coordinate -- coordenada en formato "C2" (columna, fila).
+
+        Retorna:
+        True si el disparo impacta un barco, False si impacta agua.
+        """
+        # Validar la coordenada antes de realizar el disparo
+        row, column = self.validateCoordinate(coordinate)
+
         # Verifica si el disparo impacta un barco
-        elif self.board[row][column] != 0:
+        if self.board[row][column] != 0:
             self.last_hit = self.board[row][column]
-            self.board[row][column] = "x"
-            self.player_board[row][column] = "\U0001F4A5"  # Muestra el impacto en el tablero del jugador
-            print(f"\U0001F6A2 HIT \U0001F4A5")
+            self.board[row][column] = True
+            self.player_board[row][column] = self.EMOJIS["colition"]  # Muestra el impacto en el tablero del jugador
             return True
-        
         else:
             # Disparo en el agua
-            self.player_board[row][column] = "\U0001F30A"
-            print(f"\U0001F4A6 WATER \U0001F4A6")
+            self.player_board[row][column] = self.EMOJIS["wave"]
             return False
         
-    def downedShip(self):
+    def isCoordinateAlreadyShot(self, coordinate):
+        if coordinate in self.shooting_history:
+            return True
+        else:
+            self.shooting_history.add(coordinate)
+            return False
+        
+    def isShipDowned(self):
         """
         Verifica si el último barco impactado ha sido hundido completamente.
         """
@@ -227,12 +280,8 @@ class NavalBattle:
                 if self.last_hit == self.board[h][w]:
                     return False
 
-        print("Downed Ship")
         self.ships_quantity -= 1
         # Muestra el número de impactos del barco hundido
-        for i in range(self.last_hit):
-            print("\U0001F4A5", end=" ")
-        print("\n")
         self.showInfo()
         return True
 
